7f27bd4f78575dfbbee1f92d81f2b0a5ac6f1fb2
Stas <nevmyvakastas@gmail.com>
2020-07-09 23:43:28 +0600

    used new loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import NextNprogress from 'nextjs-progressbar'
+
 export default function MyApp({Component, pageProps}){
     return (
         <> 
+            <NextNprogress
+                color="hotpink"
+                startPosition="0.3"
+                height="3"
+            />
             <Component {...pageProps}/>
             <style jsx global>
                 {`
